919f9ea1f24f96c4e5431d29365120bb4655fbd9
Victor Correal Ramos <35953130+MisterBigThor@users.noreply.github.com>
2019-12-12 17:06:26 +0100

    added files

---
+++ PEC/ledAlive.cpp
@@ -0,0 +1,15 @@
+#include "ledAlive.h"
+#include "Arduino.h"
+
+ledAlive::ledAlive(int pin){
+    pinLedAlive = pin;
+    ledAliveStatus = 0;
+}
+
+void ledAlive::actLedAlive(int forceStatus){
+    digitalWrite(pinLedAlive, ledAliveStatus>2);
+    
+    ledAliveStatus++;
+    if(ledAliveStatus >10) ledAliveStatus = 0;
+    return;
+}
